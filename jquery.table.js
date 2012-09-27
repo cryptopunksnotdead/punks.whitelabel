5d2b9187951428944fabf60f31c0261b813e3301
Gerald Bauer <gerald.bauer@gmail.com>
2012-09-27 17:00:11 +0200

    add data-sort,data-filter,etc.

--- jquery.table.js
+++ jquery.table.js
@@ -15,59 +15,73 @@
     *****  add to docu
    
 */
-  
-  function table_hovered( table_rows_selector )
-  {
-    $( table_rows_selector ).hover(function(){  
-       $(this).find('td').addClass('hovered');  
-     }, function(){  
-       $(this).find('td').removeClass('hovered');  
-     });
-  }
 
 
-
-  // todo: rename to table_filter_worker or similar?
+    var keyFuncs = {
+          "input" : function( $cell ) { return $cell.find('input[type=text]').val(); },
+          "text":   function( $cell ) { return $cell.text(); },
+          "void":   function( $cell ) { return ''; }
+        };
   
-  // todoL use keyfunc like in table_sorter  
   
-  function filter(selector, query) {  
+  
+  function table_filter_worker( $rows, query, keyFuncMap ) {  
     query =   $.trim(query); //trim white space  
     query = query.replace(/ /gi, '|'); //add OR for regex query  
-    
-    $(selector).each( function() {      
-      
-      var text = "| ";
-      var $inputs = $( 'input[type=text]', this );
 
-      if( $inputs.length == 0 )
-      {
-        $( 'td', this ).each( function() {
-         text += $(this).text() + " |";
-        });
-      }      
-      else
-      {
-        $inputs.each( function() {
-         text += $(this).val() + " |";
-        });
-      }
+    var regex = new RegExp(query, 'i');
+    
+    $rows.each( function() {      
+      var text = "| ";
+      $(this).find( 'td' ).each( function( index ) {
+        text +=  keyFuncMap[index]( $(this) ) + " |";
+      } );      
       
       // console.log( "[debug] text: " + text );
       
-      if( text.search( new RegExp(query, 'i')) < 0 ) {  
+      if( text.search( regex ) < 0 ) {  
           $(this).hide().removeClass('visible')  
       } else {  
           $(this).show().addClass('visible');  
-      }      
-    });  
+      }
+    });  // each row
   }    
 
-  function table_filter( filter_id, table_rows_selector )
-  {
-    //default each row to visible  
-    $( table_rows_selector ).addClass('visible');  
+  function table_filter( filter_id, table_id )
+  {    
+    var $table = $( table_id );
 
+    // build keyFuncMap for/from columns
+    var keyFuncMap = [];
+    
+    // NB: by default use :last (if more than one table header row; only use the last one)
+    $table.find( 'thead tr:last th' ).each( function( columnIndex ) {
+
+      // NB: lets you use data-filter=false to make column NOT filterable
+     var filterable = $(this).data( 'filter' );
+     if( filterable === undefined )
+       filterable = true;
+    
+     if( filterable )
+     {
+      var keyType = $(this).data( 'input' );
+      if( keyType === true )
+        keyType = 'input';
+      else
+        keyType = 'text';
+
+        keyFuncMap[ columnIndex ] = keyFuncs[ keyType ];
+     }
+     else
+     {
+        keyFuncMap[ columnIndex ] = keyFuncs['void']; 
+     }
+    }); // each th
+
+
+    var $rows = $table.find( 'tbody tr,tfoot tr' );    
+    $rows.addClass('visible');   //default each row to visible
+    
     $( filter_id ).keyup( function(event) {  
     //if esc is pressed or nothing is entered  
     if( event.keyCode == 27 || $(this).val() == '' ) {  
@@ -75,14 +89,15 @@
       $(this).val('');  
       //we want each row to be visible because if nothing  
       //is entered then all rows are matched.  
-      $( table_rows_selector ).removeClass('visible').show().addClass('visible');  
+      $rows.removeClass('visible').show().addClass('visible');  
     }  
     //if there is text, lets filter  
-    else {  
-      filter( table_rows_selector, $(this).val());  
+    else {
+      table_filter_worker( $rows, $(this).val(), keyFuncMap );  
     }    
     });
   }
+
   
   function table_sorter( table_id )
   {
@@ -99,46 +114,51 @@
           "float"  : function(text) { return parseFloat( text.replace( /,/g, '')); },
           "string" : function(text) { return text; }
       };    
+        
+    var $table = $( table_id );
     
-    var keyFuncs = {
-          "input" : function( $cell ) { return $cell.find('input[type=text]').val(); },
-          "text":   function( $cell ) { return $cell.text(); }
-        };
+    // NB: by default use :last (if more than one table header row; only use the last one)
+    $table.find( 'thead tr:last th' ).each( function( columnIndex ) {
 
-    $( table_id + ' thead tr.sortable td').each( function( columnIndex ) {
-
-    if( $(this).is( '.sortable' ) )
+    // NB: lets you use data-sort=false to make column NOT sortable
+    var sortable = $(this).data( 'sort' );
+    if( sortable === undefined )
+      sortable = true;
+    
+    if( sortable )
     {
       // console.log( "sortable["+columnIndex+"]" );      
 
+      // NB: add class .sortable for easy styling
+      $(this).addClass( 'sortable' );     
+   
+      var keyType = $(this).data( 'input' );
+      if( keyType === true )
+         keyType = 'input';
+      else
+         keyType = 'text';
+
+      var sortType = $(this).data( 'type' );
+      if( sortType === undefined )
+         sortType = 'string';
+    
+      // console.log( "keytype: " + keyType + ", sortType: " + sortType ); 
+    
+      var keyFunc  = keyFuncs[ keyType ];
+      var convFunc = convFuncs[ sortType ];
+      var sortFunc = sortFuncs[ sortType ];      
+      
       $(this).click( function() {
       
          // console.log( "onclick sortable["+columnIndex+"]");
-    
-         var keyType = $(this).data( 'input' );
-         if( keyType === true )
-           keyType = 'input';
-         else
-           keyType = 'text';
-
-         var sortType = $(this).data( 'type' );
-         if( sortType === undefined )
-           sortType = 'string';
-    
-         // console.log( "keytype: " + keyType + ", sortType: " + sortType ); 
-    
-         var keyFunc  = keyFuncs[ keyType ];
-         var convFunc = convFuncs[ sortType ];
-         var sortFunc = sortFuncs[ sortType ];      
       
          var sortDirection = $(this).is( '.sorted-asc' ) ? -1 : 1;
       
-         // table > thead > tr > td
-         var $rows = $(this).parent().parent().parent().find( 'tr.row.sortable' );
+         var $rows = $table.find( 'tbody tr' );
          
          // console.log( $rows );
 
-         $.each( $rows, function( index, row ) {
+         $rows.each( function( index, row ) {
             row.sortKey = convFunc( keyFunc( $(row).children( 'td' ).eq( columnIndex ) ));
             row.sortPos = index;   // NB: stable sort hack, part i - on equal use sortPos to keep stable sort with unstable sort
             // console.log( "["+index+"]" + row.sortKey );
@@ -157,15 +177,25 @@
           return result;
         });
         
-        $.each( $rows, function( index, row ) {
-          $( table_id + ' tbody' ).append( row );
+        $rows.each( function( index, row ) {
+          $table.find( 'tbody' ).append( row );
           // row.sortKey = null;
           // row.sortPos = null;
         });
         
-        $( table_id + ' thead tr.sortable td').removeClass( 'sorted-asc sorted-desc' );
+        $table.find( 'thead tr:last th').removeClass( 'sorted-asc sorted-desc' );
         sortDirection == 1 ? $(this).addClass( 'sorted-asc' ) : $(this).addClass( 'sorted-desc' );
    }); 
     }
    });
+  }
+
+  
+  function table_hovered( table_id )
+  {
+    $( table_id ).find( 'tbody tr,tfoot tr' ).hover( function() {  
+       $(this).find( 'td' ).addClass( 'hovered' );  
+     }, function() {  
+       $(this).find( 'td' ).removeClass( 'hovered' );  
+    });
   }
